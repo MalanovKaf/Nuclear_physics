3aed9d8d832ec6a01bb94dafc2a6040b1a819968
KareemIb <nekonaut69@gmail.com>
2026-02-26 18:33:31 +0300

    "Чуть-чуть менял"

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from labN2 import Figure
 import math
 import numpy as np
 
-triangle = Figure([[2, 4], [4, 6], [6, 4], [6, 2], [2, 2]])
+triangle = Figure([[2, 0], [2, 5]])
 A = B = 10
-N = 10000
+N = 1000
 ITER = 10
 
-squares = [triangle.square(A, B, N) for _ in range(ITER)]
+squares = [triangle.square(A, B, N) for i in range(ITER)]
 print("Дисперсия через numpy:", np.var(squares, ddof=1))
 print("Мат ожидание через numpy:", np.mean(squares))
 print("Площадь (Монте-Карло):", squares)
