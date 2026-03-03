41fa9c45aacef9ed6739ab9239b85cc85ea85556
MalanovKaf <losiash228007@mail.ru>
2026-03-03 23:15:27 +0300

    "Лаба 5 и ее main файл"

---
+++ LabN5.py
@@ -0,0 +1,63 @@
+import random
+from math import sqrt
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+
+class Source_photon:
+
+    def __init__(self, P1, P2, P3, E1, E2, E3, N):
+        self.P1 = abs(P1)
+        self.P2 = abs(P2)
+        self.P3 = abs(P3)
+        self.E1 = abs(E1)
+        self.E2 = abs(E2)
+        self.E3 = abs(E3)
+        self.N = abs(N)
+        if self.P1 + self.P2 + self.P3 != 1:
+            sys.exit("Сумма выходов источника должна быть 1")
+
+    def Energy_diagramma(self):
+        N1 = 0
+        N2 = 0
+        N3 = 0
+        for i in range(self.N):
+            G = random.uniform(0, 1)
+            if G < self.P1:
+                N1 += 1
+            elif G < self.P1 + self.P2:
+                N2 += 1
+            else:
+                N3 += 1
+        self.plot_histogram(N1,N2,N3)
+
+    def plot_histogram(self, N1, N2, N3):
+        """
+        Построение гистограммы распределения фотонов по энергиям
+        """
+        energies = [self.E1, self.E2, self.E3]
+        counts = [N1, N2, N3]
+
+        # Создание фигуры
+        plt.figure(figsize=(10, 6))
+
+        # Столбчатая диаграмма (количество фотонов)
+        bars = plt.bar(range(len(energies)), counts, color=['blue', 'green', 'red'],alpha=0.7)
+
+        # Настройка графика
+        plt.xlabel('Энергия фотонов')
+        plt.ylabel('Количество фотонов')
+        plt.title('Распределение фотонов по энергиям')
+        plt.xticks(range(len(energies)), [f'E1={self.E1}', f'E2={self.E2}', f'E3={self.E3}'])
+
+        # Добавление значений над столбцами
+        for bar, count in zip(bars, counts):
+            height = bar.get_height()
+            plt.text(bar.get_x() + bar.get_width() / 2., height + 0.01 * max(counts),f'{count}', ha='center', va='bottom', fontsize=11, fontweight='bold')
+
+        # Добавление сетки для лучшей читаемости
+        plt.tight_layout()
+        plt.show()
+
---
+++ main.py
@@ -0,0 +1,3 @@
+from LabN5 import *
+source = Source_photon(P1=0.2, P2=0.6, P3=0.2, E1=100, E2=200, E3=300, N=10000)
+source.Energy_diagramma()
