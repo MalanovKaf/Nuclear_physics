76d2584c45861f0bdd8489b33d49815e4d9a26b6
MalanovKaf <losiash228007@mail.ru>
2026-03-03 23:22:39 +0300

    "Изменение main"

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 from LabN5 import *
-source = Source_photon(P1=0.2, P2=0.6, P3=0.2, E1=100, E2=200, E3=300, N=10000)
-source.Energy_diagramma()
+N=[5,10,100]
+for i in range (len(N)):
+    source = Source_photon(P1=0.3, P2=0.5, P3=0.2, E1=100, E2=200, E3=300, N=N[i])
+    source.Energy_diagramma()
+
