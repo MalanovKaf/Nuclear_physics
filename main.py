1900fe01e5713078ff5a2550526614f3cc009e72
KareemIb <nekonaut69@gmail.com>
2026-03-05 18:52:13 +0300

    "Изменил"

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from LabN5 import *
-N=[5,10,100]
+N=[100,1000000]
 for i in range (len(N)):
-    source = Source_photon(P1=0.3, P2=0.5, P3=0.2, E1=100, E2=200, E3=300, N=N[i])
+    source = Source_photon(P1=0.2, P2=0.5, P3=0.3, E1=100, E2=200, E3=300, N=N[i])
     source.Energy_diagramma()
 
